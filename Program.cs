b13a2992ff78d331cde98b755ad14a9b7fbd3099
Matt Sieker <matt@siekernet.com>
2022-04-25 19:40:26 -0500

    Helps to hit save all

--- Program.cs
+++ Program.cs
@@ -12,15 +12,27 @@ public class Program
     public static void Main(string[] args)
     {
         var cb = new ConfigurationBuilder();
+
+        // This bit with sitePath is so the site.json file gets written into bin, instead of the current directory.
         var sitePath = Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "appsettings.site.json");
-        Console.WriteLine(sitePath);
         var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "development";
+
+        /*
+         * Add some preliminary JSON files for settings. Note the second true on the third one, this
+         * allows the configuration to reload on change.
+         */
         cb.AddJsonFile("appsettings.json")
             .AddJsonFile($"appsettings.{environment}.json", true)
             .AddJsonFile(sitePath, true, true);
         
+        // Make a preliminary configuration object
         var conf = cb.Build();
         
+        /*
+         * Check for a database configuration setting. For the sake of this PoC, assume the
+         * database settings are in the site-specific settings. If they don't exist, create
+         * the settings file and reload the config.
+         */
         var dbSection = conf.GetSection("Database");
         if (!dbSection.Exists())
         {
@@ -36,20 +48,33 @@ public class Program
             File.WriteAllText(sitePath, JsonSerializer.Serialize(siteSettings));
             conf.Reload();
         }
+
+        // Dump out config to show what's going on at this point.
         Console.WriteLine(conf.GetDebugView());
 
+        // Set up the DbConfigurationSource with the connection string from the config file.
         var connStr = conf["Database:SettingsConnectionString"];
         var configSource = new DbConfigurationSource(opt => opt.UseSqlite(connStr));
         cb.Add(configSource);
 
+        // The real actual configuration provider
         conf = cb.Build();
         
-
         var sc = new ServiceCollection();
 
+        // Put the configuration source into the DI container so we can get at it later
         sc.AddSingleton(configSource);
+        // Add email settings to the DI container
         sc.Configure<EmailSettings>(conf.GetSection("Email"));
 
+        /*
+         * The rest of this is just finding and running a set of steps to give some demo behavior.
+         * Before each step, the name of the step will be printed, and after it runs, the resulting configuration
+         * is printed
+         *
+         * For the POC, the order of options is write a full known configuration to the database, read it back out,
+         * update a single property on it and write it back, and then write it out again.
+         */
         Type ti = typeof(IExampleAction);
         foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
         {
